5d9084d9cb018a36c9dcc2c8863823b9519bfa27
Paul D'Angio <pcdangio@gmail.com>
2023-05-18 19:00:35 -0400

    container: initial commit of set

---
+++ src/std/container/set.hpp
@@ -0,0 +1,76 @@
+/// \file std/set.hpp
+/// \brief Defines the set::set class.
+#ifndef STD___SET_H
+#define STD___SET_H
+
+// std
+#include <std/container/container.hpp>
+
+namespace std {
+
+/// \brief A unique set of objects.
+/// \tparam object_type The type of object stored in the set.
+template <typename object_type>
+class set
+    : public std::container<object_type>
+{
+public:
+    // CONSTRUCTORS
+    /// \brief Creates a new set instance.
+    /// \param capacity The maximum capacity of the set.
+    set(size_t capacity)
+        : std::container<object_type>(capacity)
+    {}
+
+    // MODIFIERS
+    /// \brief Inserts an object in the set.
+    /// \param object The object to insert.
+    /// \return TRUE if the set now contains the object, FALSE if the set is at capacity.
+    bool insert(const object_type& object)
+    {
+        // Check if object already exists in list.
+        if(set::contains(object))
+        {
+            return true;
+        }
+
+        // Check for size.
+        if(set::m_end == set::m_capacity)
+        {
+            return false;
+        }
+
+        // Add new object and increment end pointer.
+        *set::m_end++ = object;
+
+        return true;
+    }
+    /// \brief Erases an object from the set.
+    /// \param object The object to erase.
+    void erase(const object_type& object)
+    {
+        // Search for object.
+        auto entry = set::m_begin;
+        for(; entry != set::m_end; ++entry)
+        {
+            if(*entry == object)
+            {
+                break;
+            }
+        }
+
+        // Check if object was found.
+        if(entry == set::m_end)
+        {
+            return;
+        }
+
+        // Erase at position.
+        std::container<object_type>::erase(entry);
+    }
+    using std::container<object_type>::erase;
+};
+
+}
+
+#endif
