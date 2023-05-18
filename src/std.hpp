9a60c603bf922f893857f5fe2334188856fcf5ff
Paul D'Angio <pcdangio@gmail.com>
2023-05-18 19:34:06 -0400

    package: added existing classes to primary std header

---
+++ src/std.hpp
@@ -0,0 +1,15 @@
+/// \file std.hpp
+/// \brief The primary include header for the std library.
+#ifndef STD_H
+#define STD_H
+
+// CONTAINER
+#include <std/container/container.hpp>
+#include <std/container/iterator.hpp>
+#include <std/container/set.hpp>
+#include <std/container/vector.hpp>
+
+// MEMORY
+#include <std/memory/shared_ptr.hpp>
+
+#endif
